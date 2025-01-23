2c1da07db4211270934fbe8ef2c16051af8d7c31
LongTriggered <96937586+LongTriggered@users.noreply.github.com>
2025-01-22 23:59:12 -0800

    Update FruitFinder.lua

--- FruitFinder.lua
+++ FruitFinder.lua
@@ -2,5 +2,5 @@ ServerHopTimer = 5
 TeleportSafe = true
 Webhook = "https://discord.com/api/webhooks/1330497976530899016/Fd3II8yhiM_wVjTQzPDDv_MVTAHAwtIKwCc1futvX3ztqFOe7zrWquc-wjdn0fbPDwpn"
 _G.Version="Fruit Farm"loadstring(game:HttpGet("https://raw.githubusercontent.com/Efe0626/RaitoHub/main/Script"))()
-game:GetService("RunService"):Set3dRenderingEnabled(true)
+game:GetService("RunService"):Set3dRenderingEnabled(false)
  setfpscap(10)
