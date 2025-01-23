f0ff9d32ac655007490cef8409b2b919eb54b41b
LongTriggered <96937586+LongTriggered@users.noreply.github.com>
2025-01-23 00:15:42 -0800

    Update FruitFinder.lua

--- FruitFinder.lua
+++ FruitFinder.lua
@@ -2,11 +2,7 @@ repeat wait() until game:IsLoaded()
 repeat wait() until game.Players
 repeat wait() until game.Players.LocalPlayer
 repeat wait() until game.ReplicatedStorage
-repeat wait() until game.ReplicatedStorage:FindFirstChild("Remotes");
-repeat wait() until game.Players.LocalPlayer:FindFirstChild("PlayerGui");
-repeat wait() until game.Players.LocalPlayer.PlayerGui:FindFirstChild("Main");
-repeat wait()
-until game:GetService("Players").LocalPlayer.PlayerGui:FindFirstChild("LoadingScreen") == nil 
+
 ServerHopTimer = 5
 TeleportSafe = true
 Webhook = "https://discord.com/api/webhooks/1330497976530899016/Fd3II8yhiM_wVjTQzPDDv_MVTAHAwtIKwCc1futvX3ztqFOe7zrWquc-wjdn0fbPDwpn"
