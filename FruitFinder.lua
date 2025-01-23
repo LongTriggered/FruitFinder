bd9a3488bba339b0d0cc60077daa282ef99a8d54
LongTriggered <96937586+LongTriggered@users.noreply.github.com>
2025-01-23 00:13:13 -0800

    Update FruitFinder.lua

--- FruitFinder.lua
+++ FruitFinder.lua
@@ -1,7 +1,14 @@
+repeat wait() until game:IsLoaded()
+repeat wait() until game.Players
+repeat wait() until game.Players.LocalPlayer
+repeat wait() until game.ReplicatedStorage
+repeat wait() until game.ReplicatedStorage:FindFirstChild("Remotes");
+repeat wait() until game.Players.LocalPlayer:FindFirstChild("PlayerGui");
+repeat wait() until game.Players.LocalPlayer.PlayerGui:FindFirstChild("Main");
+repeat wait()
+until game:GetService("Players").LocalPlayer.PlayerGui:FindFirstChild("LoadingScreen") == nil 
 ServerHopTimer = 5
 TeleportSafe = true
-game:GetService("RunService"):Set3dRenderingEnabled(false)
- setfpscap(10)
 Webhook = "https://discord.com/api/webhooks/1330497976530899016/Fd3II8yhiM_wVjTQzPDDv_MVTAHAwtIKwCc1futvX3ztqFOe7zrWquc-wjdn0fbPDwpn"
 _G.Version="Fruit Farm"loadstring(game:HttpGet("https://raw.githubusercontent.com/Efe0626/RaitoHub/main/Script"))()
 
