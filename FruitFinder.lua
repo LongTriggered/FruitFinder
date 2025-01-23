4006b58739b8c3cc8f10a31bc162f9b1851340ee
LongTriggered <96937586+LongTriggered@users.noreply.github.com>
2025-01-23 00:16:46 -0800

    Update FruitFinder.lua

--- FruitFinder.lua
+++ FruitFinder.lua
@@ -2,7 +2,15 @@ repeat wait() until game:IsLoaded()
 repeat wait() until game.Players
 repeat wait() until game.Players.LocalPlayer
 repeat wait() until game.ReplicatedStorage
-
+  local args = {
+        [1] = "SetTeam",
+        [2] = "Pirates"
+    }
+    game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer(unpack(args)) 
+    local args = {
+        [1] = "BartiloQuestProgress"
+    }
+    game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer(unpack(args))
 ServerHopTimer = 5
 TeleportSafe = true
 Webhook = "https://discord.com/api/webhooks/1330497976530899016/Fd3II8yhiM_wVjTQzPDDv_MVTAHAwtIKwCc1futvX3ztqFOe7zrWquc-wjdn0fbPDwpn"
