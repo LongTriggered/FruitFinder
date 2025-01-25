f6087925e60306f6f28819f25b3e04b0359d549d
LongTriggered <96937586+LongTriggered@users.noreply.github.com>
2025-01-25 00:52:58 -0800

    Update FruitFinder.lua

--- FruitFinder.lua
+++ FruitFinder.lua
@@ -1,4 +1,4 @@
-repeat wait() until gameIsLoaded()
+repeat wait() until game:IsLoaded()
 repeat wait() until game.Players
 repeat wait() until game.Players.LocalPlayer
 repeat wait() until game.ReplicatedStorage
@@ -6,9 +6,9 @@ repeat wait() until game.ReplicatedStorage:FindFirstChild("Remotes");
 repeat wait() until game.Players.LocalPlayer:FindFirstChild("PlayerGui");
 repeat wait() until game.Players.LocalPlayer.PlayerGui:FindFirstChild("Main (minimal)");
 repeat wait()
-until gameGetService(Players).LocalPlayer.PlayerGuiFindFirstChild(LoadingScreen) == nil
+until game:GetService("Players").LocalPlayer.PlayerGui:FindFirstChild("LoadingScreen") == nil
 setfpscap(7)
-gameGetService(RunService)Set3dRenderingEnabled(false)
+game:GetService("RunService"):Set3dRenderingEnabled(false)
   local args = {
         [1] = "SetTeam",
         [2] = "Marines"
@@ -20,7 +20,7 @@ gameGetService(RunService)Set3dRenderingEnabled(false)
     game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer(unpack(args))
 ServerHopTimer = 6
 TeleportSafe = true
-Webhook = ""
+Webhook = "https://discord.com/api/webhooks/1330497976530899016/Fd3II8yhiM_wVjTQzPDDv_MVTAHAwtIKwCc1futvX3ztqFOe7zrWquc-wjdn0fbPDwpn"
 _G.Version="Fruit Farm"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/SaltyHB/Poggers/refs/heads/main/Main"))()
 
